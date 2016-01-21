a2e2fe3a36af2fcef8e81c5214313a297401adde
MuffyPuff <sjjeuseen@gmail.com>
2016-01-21 18:44:30 +0100

    Create isPrimeSOE.cpp

---
+++ isPrimeSOE.cpp
@@ -0,0 +1,20 @@
+#include <cstdio>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	unsigned int n;
+	scanf("%u",&n);
+	vector<bool>isPrime(n,1);//lim ~8e6?
+
+	for(unsigned int i=2;i*i<n;++i){
+		if(isPrime[i]==0) continue;
+		isPrime[i]=0;
+		for(unsigned int j=i*i;j<n;j+=i) isPrime[j]=0;
+	}
+
+	if(isPrime[n]==1) printf("True");
+
+	return 0;
+}
